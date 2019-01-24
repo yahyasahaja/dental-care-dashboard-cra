5d56de80b15af259d37c90e0f4ecf0485e6251b4
Yahya Sahaja <yahya.sahaja2@gmail.com>
2019-01-24 19:27:19 +0700

    add build folder

---
+++ build/precache-manifest.77673ec60906d8fd1e195dbcfc0fde94.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c39679e7ea6c5a58491",
+    "url": "/static/css/Dashboard.fbfbf72e.chunk.css"
+  },
+  {
+    "revision": "90b38a7c92d663a7046f",
+    "url": "/static/js/0.90b38a7c.chunk.js"
+  },
+  {
+    "revision": "9edda1317156294666ab",
+    "url": "/static/js/main.9edda131.chunk.js"
+  },
+  {
+    "revision": "7e87fc213d8722ffa5a2",
+    "url": "/static/js/runtime~main.7e87fc21.js"
+  },
+  {
+    "revision": "dde4bbfb61302e5eea9c",
+    "url": "/static/js/Login.dde4bbfb.chunk.js"
+  },
+  {
+    "revision": "f9dca29640c12307a87f",
+    "url": "/static/js/5.f9dca296.chunk.js"
+  },
+  {
+    "revision": "0c39679e7ea6c5a58491",
+    "url": "/static/js/Dashboard.0c39679e.chunk.js"
+  },
+  {
+    "revision": "c7d9c0c9941ecf820cd1",
+    "url": "/static/js/4.c7d9c0c9.chunk.js"
+  },
+  {
+    "revision": "9edda1317156294666ab",
+    "url": "/static/css/main.58236888.chunk.css"
+  },
+  {
+    "revision": "dde4bbfb61302e5eea9c",
+    "url": "/static/css/Login.b2d5f2e6.chunk.css"
+  },
+  {
+    "revision": "82d75ce0ad4c13f5b22ff63dfc40fe80",
+    "url": "/index.html"
+  }
+];
